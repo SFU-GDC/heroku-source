3f0e0c354c95a4019e047fce04a29f31de76ba8f
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-05-15 00:09:42 -0700

    Finally got role adding to work

--- bot/cogs/roles.py
+++ bot/cogs/roles.py
@@ -16,64 +16,41 @@ class Roles(commands.Cog):
 
     @commands.command()
     async def notify(self, ctx, activate):
-        print("notify")
         val = str(activate).lower()
         if val == "true":
             await ctx.send("You will now recieve notifications")
+            await add_role(ctx.message.author, ctx.guild.roles, "Notification Squad")
+
             message = await ctx.send("Choose a colour by reacting to this message")
             await message.add_reaction(emoji=bulbasaur_green)
             await message.add_reaction(emoji=gameboy_yellow)
             is_bulbasaur = lambda reaction, user: user == ctx.message.author and str(reaction.emoji) == bulbasaur_green
             is_gameboy = lambda reaction, user: user == ctx.message.author and str(reaction.emoji) == gameboy_yellow
             check = lambda r, u: is_gameboy(r,u) or is_bulbasaur(r,u)
-            print(ctx.guild.roles)
-    
-            role = discord.utils.get(ctx.guild.roles, name="Notification Squad")
-            print(role)
-            rol2e = discord.utils.get(ctx.guild.roles, id=842605912241209355)
-            print(rol2e)
-            await ctx.message.author.add_roles(role)
 
             # wait for reponse or timeout
             try:
-                print("waiting")
-                print(ctx.guild.roles)
                 reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=45.0)
                 user = ctx.message.author
 
-                print("react")
-                print(reaction.emoji)
-                print(reaction.emoji.id)
-                print(reaction.emoji.name)
                 if reaction.emoji.name == "bulbasaur":
-                    role = discord.utils.get(ctx.guild.roles, name="Bulbasaur Green")
-                    print(role)
-                    await user.add_roles(role)
+                    await add_role(user, ctx.guild.roles, "Bulbasaur Green")
+                    await remove_role(user, "Gameboy Yellow")
                 elif reaction.emoji.name == "gameboy":
-                    role = discord.utils.get(ctx.guild.roles, name="Gameboy Yellow")
-                    await user.add_roles(role)             
+                    await add_role(user, ctx.guild.roles, "Gameboy Yellow")
+                    await remove_role(user, "Bulbasaur Green")
             except asyncio.TimeoutError:
-                await ctx.send('You took too long, try again.')
+                await ctx.send('You took too long to pick a colour, try again if you want to claim one.')
             else:
-                print("end")
-
                 await ctx.send("Enjoy {}!".format(reaction.emoji))
 
         elif val == "false":
             # remove roles if possible
             user = ctx.message.author
-            print(user.roles)
-            if "Bulbasaur Green" in user.roles:
-                role = discord.utils.get(ctx.guild.roles, name="Bulbasaur Green")
-                await user.add_roles(role)
 
-            if "Gameboy Yellow" in user.roles:
-                role = discord.utils.get(ctx.guild.roles, name="Gameboy Yellow")
-                await user.add_roles(role)
-
-            if "Notification Squad" in user.roles:
-                role = discord.utils.get(ctx.guild.roles, name="Notification Squad")
-                await user.add_roles(role)
+            await remove_role(user, "Bulbasaur Green")
+            await remove_role(user, "Gameboy Yellow")
+            await remove_role(user, "Notification Squad")
 
             await ctx.send("You will no longer recieve notifications")
         else:
@@ -84,6 +61,17 @@ class Roles(commands.Cog):
         print("error: {}".format(repr(error)))
         await ctx.send("Oops, something went wrong. Call the function like this: `,notify true`")
     
+    
+# --------------------------------------------------------------------------- #
+
+async def remove_role(user, name):
+    r = discord.utils.get(user.roles, name=name)
+    if r: await user.remove_roles(r)
+
+async def add_role(user, roles, name):
+    r = discord.utils.get(roles, name=name)
+    if r: await user.add_roles(r)
+
 
 # --------------------------------------------------------------------------- #
 
