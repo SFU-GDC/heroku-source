86154f48cda477d77831596bb3055bedc6f768c3
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-06-19 12:54:02 -0700

    Update db_manager.py

--- bot/db_manager.py
+++ bot/db_manager.py
@@ -8,6 +8,7 @@ def reset_events_table():
     conn = None
     try:
         conn = psycopg2.connect(DATABASE_URL) # , sslmode='require'
+        conn.autocommit = 1
 
         cur = conn.cursor()
         cur.execute("CREATE TABLE events (name VARCHAR(255) UNIQUE)") #, datetime TIMESTAMP, description VARCHAR(1024));")
@@ -34,8 +35,10 @@ def add_event(unique_event_name, date, desc):
     print("INSERT INTO events VALUES ({})".format(unique_event_name)) #, %s, %s)", (unique_event_name, date, desc))
     try:
         conn = psycopg2.connect(DATABASE_URL) # , sslmode='require'
+        conn.autocommit = 1
 
         cur = conn.cursor()
+        cur.execute("CREATE TABLE events (name VARCHAR(255))")
         cur.execute("INSERT INTO events VALUES ({})".format(unique_event_name)) #, %s, %s)", (unique_event_name, date, desc))
         cur.close()
     except Exception as error:
