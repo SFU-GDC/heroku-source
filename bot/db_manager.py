dac1d35acdba7791f4798687d6b7f4d5ef0b0191
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-06-19 13:01:59 -0700

    Update db_manager.py

--- bot/db_manager.py
+++ bot/db_manager.py
@@ -11,7 +11,11 @@ def reset_events_table():
         conn.autocommit = 1
 
         cur = conn.cursor()
+        
+        # https://www.psycopg.org/docs/usage.html#adaptation-of-python-values-to-sql-types
+        cur.execute("DROP TABLE IF EXISTS events")
         cur.execute("CREATE TABLE events (name VARCHAR(255) UNIQUE)") #, datetime TIMESTAMP, description VARCHAR(1024));")
+        
         cur.close()
     except Exception as error:
         print('Could not connect to the Database.')
@@ -22,9 +26,6 @@ def reset_events_table():
             conn.close()
             print('Database connection closed.')
 
-
-    #cur.execute("DROP TABLE IF EXISTS \"events\";")
-    # https://www.psycopg.org/docs/usage.html#adaptation-of-python-values-to-sql-types
     #cur.execute("CREATE TABLE events (name VARCHAR(255) UNIQUE);") #, datetime TIMESTAMP, description VARCHAR(1024));")
 
 def remove_event(unique_event_name):
@@ -32,13 +33,11 @@ def remove_event(unique_event_name):
 
 def add_event(unique_event_name, date, desc):
     conn = None
-    print("INSERT INTO events VALUES ({})".format(unique_event_name)) #, %s, %s)", (unique_event_name, date, desc))
     try:
         conn = psycopg2.connect(DATABASE_URL) # , sslmode='require'
         conn.autocommit = 1
 
         cur = conn.cursor()
-        cur.execute("CREATE TABLE events (name VARCHAR(255))")
         cur.execute("INSERT INTO events (name) VALUES (%s)", ["meeting"]) #, %s, %s)", (unique_event_name, date, desc))
         cur.close()
     except Exception as error:
