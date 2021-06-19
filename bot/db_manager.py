2a67b3ed19250d66a7592d1d61f8861a50aeef6a
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-06-19 12:19:28 -0700

    add quotes to sql statments

--- bot/db_manager.py
+++ bot/db_manager.py
@@ -7,9 +7,9 @@ def reset_events_table():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
 
-    cur.execute("DROP TABLE IF EXISTS public.events;")
+    cur.execute("DROP TABLE IF EXISTS public.\"events\";")
     # https://www.psycopg.org/docs/usage.html#adaptation-of-python-values-to-sql-types
-    cur.execute("CREATE TABLE public.events (name VARCHAR(256) UNIQUE, datetime TIMESTAMP, description VARCHAR(1024));")
+    cur.execute("CREATE TABLE public.\"events\" (name VARCHAR(256) UNIQUE, datetime TIMESTAMP, description VARCHAR(1024));")
 
     cur.close()
     conn.close()
@@ -22,7 +22,7 @@ def add_event(unique_event_name, date, desc):
     cur = conn.cursor()
 
     #(name, datetime, description)
-    cur.execute("INSERT INTO public.events VALUES (%s, %s, %s)", (unique_event_name, date, desc))
+    cur.execute("INSERT INTO public.\"events\" VALUES (%s, %s, %s)", (unique_event_name, date, desc))
     
     cur.close()
     conn.close()
