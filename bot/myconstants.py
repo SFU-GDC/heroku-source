bb3b6254f46551fadd90e069f5f7cb44fb082e26
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-07-15 22:04:17 -0700

    uid fixes

--- bot/myconstants.py
+++ bot/myconstants.py
@@ -5,13 +5,13 @@ mynames = ["CubeBot", "CubeBot#8136"]
 bulbasaur_green = "<:bulbasaur:842983900992241666>"
 gameboy_yellow = "<:gameboy:842983577800671264>"
 
-jellyfish_blue = "<:jellyfish:865400235492114452>"
-sunlight_yellow = "<:sunlight:865447157649506354>"
-waluigi_purple = "<:wa:865452762983825408>"
-mountain_flower_purple = "<:mountain_flower:865405273987874868>"
-tallgrass_green = "<:tallgrass:865400932074127360>"
-factory_orange = "<:factory:865441219428876328>"
-crate_brown = "<:crate:865448699014545428>"
+jellyfish_blue = "<:jellyfish:843323219478052884>"
+sunlight_yellow = "<:sunlight:865445573313429524>"
+waluigi_purple = "<:wa:865452665578979329>"
+mountain_flower_purple = "<:mountain_flower:865405232099229716>"
+tallgrass_green = "<:tallgrass:865400601248792606>"
+factory_orange = "<:factorio:865441166291894332>"
+crate_brown = "<:crate:865448635100692510>"
 
 color_emote_list = [
     jellyfish_blue, sunlight_yellow, 
@@ -21,12 +21,12 @@ color_emote_list = [
 color_emote_name_list = [
     "jellyfish", "sunlight", 
     "wa", "mountain_flower", "tallgrass", 
-    "factory", "crate"
+    "factorio", "crate"
 ]
 color_list = [
     "Jellyfish Blue", "Sunlight Yellow", 
     "Sea of Greed Purple", "Mountain Flower Purple", "Tall Grass Green", 
-    "Factory Orange", "Crate Brown"
+    "Factorio Orange", "Crate Brown"
 ]
 
 extended_color_emote_list = color_emote_list + [bulbasaur_green, gameboy_yellow]
